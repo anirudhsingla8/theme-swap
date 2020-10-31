3af5b15491fbffeadcb38154439daec07c4130d9
Anirudh Singla <anirudh.singla@crownit.in>
2020-10-31 16:52:27 +0530

    fixes

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -8,24 +8,47 @@ describe('Local', () => {
     it('page successfully loads', () => {
         cy.visit('/index.html')
     })
-    it('theme swap', () => {
-        cy.get('p').contains('I am a Newton School student, ready to become an awesome full stack developer')
+})
 
+describe('Local', () => {
+    it('Checking text', () => {
+        cy.get('p').contains('I am a Newton School student, ready to become an awesome full stack developer')
+    })
+})
+
+
+describe('Local', () => {
+    it('theme swap', () => {
         cy.get('div').should('have.id','app')
         cy.get('div').should('have.class','day')
+    })
+})
 
+
+describe('Local', () => {
+    it('theme swap', () => {
         cy.get('button').should('have.id','swap')
         cy.get('button').should('have.class','button_day')
+    })
+})
 
+
+
+describe('Local', () => {
+    it('theme swap', () => {
         cy.get('button').click().then(()=>{
             cy.get('#app').should('have.class','night')
             cy.get('#swap').should('have.class','button_night')
         })
+    })
+})
 
+
+describe('Local', () => {
+    it('theme swap', () => {
         cy.get('button').click().then(()=>{
             cy.get('#app').should('have.class','day')
             cy.get('#swap').should('have.class','button_day')
         })
-
     })
 })
